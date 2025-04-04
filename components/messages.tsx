f23276db62c4b8181f287093fd60ff6f70d6ed98
nanameru <4869nanataitai@gmail.com>
2025-04-04 14:48:45 +0900

    UI改善: メッセージ表示エリアの最大幅を768pxに設定

--- components/messages.tsx
+++ components/messages.tsx
@@ -180,7 +180,7 @@ function PureMessages({
   return (
     <div
       ref={messagesContainerRef}
-      className="flex flex-col min-w-0 gap-6 flex-1 overflow-y-scroll pt-4"
+      className="flex flex-col min-w-0 gap-6 flex-1 overflow-y-scroll pt-4 max-w-[768px] mx-auto w-full"
     >
       {sortedMessages.map((message, index) => (
         <PreviewMessage
